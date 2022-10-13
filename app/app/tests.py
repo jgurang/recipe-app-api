40c928f89db305aac0622f123d86c42c9c8f3a62
John Urang <jgurang@gmail.com>
2022-10-13 11:53:01 -0700

    Fixed linting errors?

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,9 +6,11 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
     """Test the calc module."""
-    
+
     def test_add_numbers(self):
         """test adding numbers together"""
         res = calc.add(5, 6)
